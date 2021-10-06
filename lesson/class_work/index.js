7e6ee5446ad0607fb49061d3413dab238fc6d94a
ilyaPronin <silverbolt777@yandex.ru>
2021-10-06 15:21:19 +0300

    первый урок

--- lesson/class_work/index.js
+++ lesson/class_work/index.js
@@ -1,5 +1,35 @@
-console.log("привет я из  nodejs");
+// console.log("привет я из  nodejs");
 
-const userName = "ilya";
+// const userName = "ilya";
+
+// console.log(userName);
+
+console.log("hello world");
+
+const myName = "Илья!";
+
+let myAge = 31;
+
+console.log(myName, myAge);
+
+let helloUser = `hello ${myName}`;
+
+let isAdmin = false;
+
+let isOpen = true;
+
+let userName = null;
+
+let myWife;
+
+console.log(helloUser);
+
+console.log(isAdmin);
+
+console.log(isOpen);
 
 console.log(userName);
+
+console.log(myWife);
+
+console.log(typeof myAge);
