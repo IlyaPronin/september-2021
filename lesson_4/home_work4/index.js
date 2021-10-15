8ba0e4cfe2ae53f4feb9663b77be9bdafac3787b
ilyaPronin <silverbolt777@yandex.ru>
2021-10-15 17:10:20 +0300

    домашняя работа4

---
+++ lesson_4/home_work4/index.js
@@ -0,0 +1,74 @@
+// let a = -3;
+// if (a == 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = 0;
+// if (a > 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = 1;
+// if (a < 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = -3;
+// if (a >= 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = 0;
+// if (a <= 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = -3;
+// if (a != 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = "тест";
+// if (a == "test") {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = 3;
+// if (a === 1) {
+//   console.log("Верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let test = 0;
+// if (test == true) {
+//   console.log("верно");
+// } else {
+//   console.log("неверно");
+// }
+// let test = 1;
+// let result = test == true ? console.log("верно") : console.log("неверно");
+
+// let test = false;
+// if (test != true) {
+//   console.log("верно");
+// } else {
+//   console.log("неверно");
+// }
+
+let test = true;
+let result = test != true ? console.log("верно") : console.log("неверно");
