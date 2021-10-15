9ab09fab45c2ab63002785c746f4c380704b2b58
ilyaPronin <silverbolt777@yandex.ru>
2021-10-15 17:51:46 +0300

    switch

--- lesson_4/home_work4/index.js
+++ lesson_4/home_work4/index.js
@@ -70,5 +70,51 @@
 //   console.log("неверно");
 // }
 
-let test = true;
-let result = test != true ? console.log("верно") : console.log("неверно");
+// let test = true;
+// let result = test != true ? console.log("верно") : console.log("неверно");
+
+// let a = 2;
+// if (a > 0 && a < 5) {
+//   console.log("верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let a = -3;
+// if (a == 0 || a == 2) {
+//   console.log((a = a + 7));
+// } else {
+//   console.log((a = a / 10));
+// }
+
+// let a = 0;
+// let b = 6;
+// if (a <= 1 && b >= 3) {
+//   console.log((a = a + b));
+// } else {
+//   console.log((a = a - b));
+// }
+
+// let a = 0;
+// let b = 9;
+// if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+//   console.log("верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// let num = 2;
+// switch (num) {
+//   case 1:
+//     console.log("зима");
+//     break;
+//   case 2:
+//     console.log("весна");
+//     break;
+//   case 3:
+//     console.log("лето");
+//     break;
+//   case 4:
+//     console.log("осень");
+//     break;
+// }
